01fac9dd8e0c452c8ab9bb1154d76a0276ba104c
Martin Craig <martin.craig@nottingham.ac.uk>
2026-04-24 16:41:25 +0100

    formatted

--- scripts/extract_slice_specifications.py
+++ scripts/extract_slice_specifications.py
@@ -1,5 +1,6 @@
 import json
 import sys
+
 import numpy as np
 
 with open(sys.argv[1], "r") as f:
@@ -10,10 +11,9 @@ sorted_indices = sorted(range(len(slice_timing)), key=lambda i: slice_timing[i])
 sorted_values = np.array([slice_timing[i] for i in sorted_indices])
 
 # Number of bands
-nb = len(sorted_indices) // (len(np.unique(sorted_values[sorted_values !=0])) + 1)
+nb = len(sorted_indices) // (len(np.unique(sorted_values[sorted_values != 0])) + 1)
 
 reshaped_indices = np.reshape(sorted_indices, [-1, nb])
 with open(sys.argv[2], "w") as f:
     for vals in reshaped_indices:
         f.write(" ".join([str(v).rjust(3) for v in vals]) + "\n")
-
