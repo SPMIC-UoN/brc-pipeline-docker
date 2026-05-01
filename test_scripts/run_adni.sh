1f3d9479be6076d69df4b2869a5651b1b98b9845
Martin Craig <martin.craig@nottingham.ac.uk>
2026-05-01 10:00:55 +0100

    Add t2 and tbss

--- test_scripts/run_adni.sh
+++ test_scripts/run_adni.sh
@@ -4,30 +4,40 @@ DATADIR=/home/bbzmsc/code/brc-pipeline-docker/_test_data
 OUTDIR=/home/bbzmsc/code/brc-pipeline-docker/_test_output
 SUBJID=002_S_6103
 
-docker run -it -v $DATADIR:/data $OUTDIR:/output martincraig/brc-pipeline struc_preproc \
+docker run -it -v $DATADIR:/data -v $OUTDIR:/output \
+    martincraig/brc-pipeline \
+    struc_preproc \
     --path /output/ \
     --subject $SUBJID \
     --input /data/$SUBJID/raw/Accelerated_Sagittal_MPRAGE_\(MSV21\)_Si.nii.gz \
+    --t2 /data/$SUBJID/raw/Sagittal_3D_FLAIR_\(MSV22\)_Si.nii.gz \
     --freesurfer \
+    --subseg \
     --qc \
     --regtype 3
 
-docker run -it -v $DATADIR:/data $OUTDIR:/output martincraig/brc-pipeline dMRI_preproc \
-    --path /output/ \
-    --subject $SUBJID \
-    --input /data/$SUBJID/raw/Axial_MB_DTI_PA_\(MSV21\)_Si.nii.gz \
-    --input_2 /data/$SUBJID/raw/Axial_MB_DTI_AP_\(MSV21\)_Si.nii.gz \
-    --slspec /data/$SUBJID/raw/Axial_MB_DTI_PA_\(MSV21\)_Si.json \
-    --pe_dir 2 \
-    --echospacing 0.00055 \
-    --p_im 1 \
-    --dtimaxshell 1000 \
-    --qc \
-    --reg \
-    --noddi
 
-docker run -it -v $PWD:/data martincraig/brc-pipeline idp_extract \
-    --in /data/adni_subjids.txt \
+docker run -it -v $DATADIR:/data -v $OUTDIR:/output \
+   --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+   martincraig/brc-pipeline \
+   dMRI_preproc \
+   --path /output/ \
+   --subject $SUBJID \
+   --input /data/$SUBJID/raw/Axial_MB_DTI_PA_\(MSV21\)_Si.nii.gz \
+   --input_2 /data/$SUBJID/raw/Axial_MB_DTI_AP_\(MSV21\)_Si.nii.gz \
+   --slspec /data/$SUBJID/raw/Axial_MB_DTI_PA_\(MSV21\)_Si.json \
+   --pe_dir 2 \
+   --echospacing 0.00055 \
+   --p_im 1 \
+   --dtimaxshell 1000 \
+   --qc \
+   --reg \
+   --tbss \
+   --noddi
+
+docker run -it -v $DATADIR:/data -v $OUTDIR:/output \
+    martincraig/brc-pipeline \
+    idp_extract \
+    --in /data/adni_subjects.txt \
     --indir /output/ \
     --outdir /output/idps
-
